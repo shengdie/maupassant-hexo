bc5aafd21ae029462ca1ace5b6c4b4c867559e26
shengdie <shilishengdie@gmail.com>
2016-10-13 00:09:02 -0400

    Add cclicense and fix nofancybox

--- source/js/fancybox.js
+++ source/js/fancybox.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('img').each(function() {
     if ($(this).parent().hasClass('fancybox')) return;
     if ($(this).hasClass('nofancybox')) return;
+    if ($(this).parent().hasClass('nofancybox') return;
     var alt = this.alt;
     if (alt) $(this).after('<span class="caption">' + alt + '</span>');
     $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" title="' + alt + '" class="fancybox"></a>');
